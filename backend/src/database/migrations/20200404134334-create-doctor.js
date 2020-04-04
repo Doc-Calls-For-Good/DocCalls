03ac74742754e5fe859e81e7ed2ad5bd8c33e9d6
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-04-04 11:29:44 -0300

    add eslint rules

--- backend/src/database/migrations/20200404134334-create-doctor.js
+++ backend/src/database/migrations/20200404134334-create-doctor.js
@@ -4,5 +4,5 @@ module.exports = {
     id: Sequelize.INTEGER,
   }),
 
-  down: (queryInterface, Sequelize) => queryInterface.dropTable('users'),
+  down: (queryInterface) => queryInterface.dropTable('users'),
 };
