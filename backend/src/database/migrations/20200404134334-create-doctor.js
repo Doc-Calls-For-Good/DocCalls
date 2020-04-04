de87063d57bc993c2a9870680454ee6ddf1f75be
Alex Eduardo Freitag <alex.f@quantich.com.br>
2020-04-04 11:58:24 -0300

    create table doctors

--- backend/src/database/migrations/20200404134334-create-doctor.js
+++ backend/src/database/migrations/20200404134334-create-doctor.js
@@ -1,8 +1,51 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => queryInterface.createTable('doctors', {
-    id: Sequelize.INTEGER,
+    id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: Sequelize.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    cpf: {
+      type: Sequelize.INTEGER,
+      unique: true,
+      allowNull: false,
+    },
+    phone: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    city: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    uf: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    specialty: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    created_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    updated_at: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
   }),
 
-  down: (queryInterface) => queryInterface.dropTable('users'),
+  down: (queryInterface) => queryInterface.dropTable('doctors'),
 };
